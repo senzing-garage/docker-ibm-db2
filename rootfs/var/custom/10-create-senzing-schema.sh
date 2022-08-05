66688b10f5360e3bd258e1f3845b6b4637482cf5
Michael Dockter <docktermj@users.noreply.github.com>
2022-08-05 10:47:17 -0400

    Issue 14.dockter.1 (#26)
    * #14 Add db2start
    
    * #14 Add log records
    
    * #14 Cache bust

--- rootfs/var/custom/10-create-senzing-schema.sh
+++ rootfs/var/custom/10-create-senzing-schema.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+echo "10.1 Starting Db2..."
+
+su - db2inst1 -c "
+  db2start;
+"
+
+echo "10.2 Db2 Started."
+
 su - db2inst1 -c "
   db2 create database g2 using codeset utf-8 territory us;
   db2 connect to g2;
   db2 -tf /opt/senzing/g2/resources/schema/g2core-schema-db2-create.sql;
   db2 connect reset;
 "
+
+echo "10.3 Complete."
